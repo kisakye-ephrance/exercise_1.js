6f2edffcb63f8835661f037275329168c922f8f0
kisakye_ephrance <nanfumaephrance@gmail.com>
2020-04-03 16:05:58 +0300

    js frist assignment

---
+++ exercise_1.js
@@ -0,0 +1,12 @@
+const grade = 53;
+if(grade >80){
+    console.log("You got an A");
+}else
+if(grade >=65 && grade<80){
+    console.log("You got a B");
+}else
+if(grade >=50 && grade<65){
+    console.log("You passed");
+}else{
+console.log("You failed the test");
+}
